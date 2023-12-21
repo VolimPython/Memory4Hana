7eda52ed11459b79456fa4a1ae7885c6a6344c8b
VolimPython <148277052+VolimPython@users.noreply.github.com>
2023-12-21 14:48:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,22 +13,22 @@ playerLivesCount.textContent=playerLives;
   //We generate the object 🧑🏻‍💻
 const getData = () => [
 
-    { imgSrc: "./images/beatles.jpeg", id: 1, name: "beatles" },
-    { imgSrc: "./images/blink182.jpeg", id: 2, name: "blink 182" },
-    { imgSrc: "./images/fkatwigs.jpeg", id: 3, name: "fka twigs" },
-    { imgSrc: "./images/fleetwood.jpeg", id: 4, name: "fleetwood" },
-    { imgSrc: "./images/joy-division.jpeg", id: 5, name: "joy division" },
-    { imgSrc: "./images/ledzep.jpeg", id: 6, name: "led zeppelin" },
-    { imgSrc: "./images/metallica.jpeg", id: 7, name: "metallica" },
-    { imgSrc: "./images/pinkfloyd.jpeg", id: 8, name: "pink floyd" },
-    { imgSrc: "./images/beatles.jpeg", id: 9, name: "beatles" },
-    { imgSrc: "./images/blink182.jpeg", id: 10, name: "blink 182" },
-    { imgSrc: "./images/fkatwigs.jpeg", id: 11, name: "fka twigs" },
-    { imgSrc: "./images/fleetwood.jpeg", id: 12, name: "fleetwood" },
-    { imgSrc: "./images/joy-division.jpeg", id: 13, name: "joy division" },
-    { imgSrc: "./images/ledzep.jpeg", id: 14, name: "led zeppelin" },
-    { imgSrc: "./images/metallica.jpeg", id: 15, name: "metallica" },
-    { imgSrc: "./images/pinkfloyd.jpeg", id: 16, name: "pink floyd" },
+    { imgSrc: "./Images/beatles.jpeg", id: 1, name: "beatles" },
+    { imgSrc: "./Images/blink182.jpeg", id: 2, name: "blink 182" },
+    { imgSrc: "./Images/fkatwigs.jpeg", id: 3, name: "fka twigs" },
+    { imgSrc: "./Images/fleetwood.jpeg", id: 4, name: "fleetwood" },
+    { imgSrc: "./Images/joy-division.jpeg", id: 5, name: "joy division" },
+    { imgSrc: "./Images/ledzep.jpeg", id: 6, name: "led zeppelin" },
+    { imgSrc: "./Images/metallica.jpeg", id: 7, name: "metallica" },
+    { imgSrc: "./Images/pinkfloyd.jpeg", id: 8, name: "pink floyd" },
+    { imgSrc: "./Images/beatles.jpeg", id: 9, name: "beatles" },
+    { imgSrc: "./Images/blink182.jpeg", id: 10, name: "blink 182" },
+    { imgSrc: "./Images/fkatwigs.jpeg", id: 11, name: "fka twigs" },
+    { imgSrc: "./Images/fleetwood.jpeg", id: 12, name: "fleetwood" },
+    { imgSrc: "./Images/joy-division.jpeg", id: 13, name: "joy division" },
+    { imgSrc: "./Images/ledzep.jpeg", id: 14, name: "led zeppelin" },
+    { imgSrc: "./Images/metallica.jpeg", id: 15, name: "metallica" },
+    { imgSrc: "./Images/pinkfloyd.jpeg", id: 16, name: "pink floyd" },
     ];
   
 
